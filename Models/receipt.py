855d27477876cba16df65a6495d29645c46025bf
ruc98 <rahul.chakwate@gmail.com>
2025-01-19 00:07:34 -0600

    Add regex pattern validation

--- Models/receipt.py
+++ Models/receipt.py
@@ -1,4 +1,5 @@
 import uuid
+import re
 
 class Receipt:
     def __init__(self, receipt_json):
@@ -6,13 +7,42 @@ class Receipt:
         self.retailer = receipt_json['retailer']
         self.purchase_date = receipt_json['purchaseDate']
         self.purchase_time = receipt_json['purchaseTime']
-        self.items = receipt_json['items']
+
+        self.items = []
+        for item in receipt_json['items']:
+            self.items.append(ReceiptItem(item))
         self.total = receipt_json['total']
         self.id = str(uuid.uuid4())
         self.score = None
 
     def validReceipt(self, receipt_json):
+        validPatterns = {
+            'retailer': r'^[\w\s\-&]+$',
+            'purchaseDate': r'^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$',
+            'purchaseTime': r'^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$',
+            'total': r'^\d+\.\d{2}$',
+        }
         for key in ['retailer','purchaseDate','purchaseTime','items','total']:
             if key not in receipt_json:
+                return False
+            if key in validPatterns and not re.match(validPatterns[key], receipt_json[key]):
+                return False
+        return isinstance(receipt_json['items'], list) and len(receipt_json['items']) > 0
+
+class ReceiptItem:
+    def __init__(self, item_data):
+        assert self.validItem(item_data)
+        self.shortDescription = item_data['shortDescription']
+        self.price = item_data['price']
+
+    def validItem(self, item_data):
+        validPatterns = {
+            'shortDescription': r'^[\w\s\-]+$',
+            'price': r'^\d+\.\d{2}$',
+        }
+        for key in ['shortDescription','price']:
+            if key not in item_data:
+                return False
+            if not re.match(validPatterns[key], item_data[key]):
                 return False
         return True
